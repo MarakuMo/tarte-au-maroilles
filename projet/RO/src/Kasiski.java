c93210765fd1b0cef2cccc0f857adf2ec9164fe1
auberi.lecocq@gmail.com <auberi.lecocq@gmail.com@859fc04b-3433-1d92-a115-270b1cbf05d0>
2011-05-14 20:47:53 +0000

    Calcul de la longueur de la clé correct cette fois-ci. Attention : ne pas dépasser 15 pour tester les longueurs.

--- projet/RO/src/Kasiski.java
+++ projet/RO/src/Kasiski.java
@@ -3,16 +3,30 @@ import java.util.HashMap;
 public class Kasiski {
 
 	// 1 : Trouver la longueur n de la clé
+	/*
+	 * Ne pas dépasser des tests de clés de longueur 15. <=> 2 < m < 15
+	 */
 	public static int longueurCle(Text t, int m) {
 		HashMap<Integer, Float> hmIC = remplirIC(t, m);
-		int maxLongCle = getMaxLongueurCle(t, m, hmIC);
-		Integer[] v = estPic(hmIC);
-		int i = 0;
-		while (v[i] == 0) {
-			i++;
+		return epurer(estPic(hmIC));
+	}
+
+	private static int epurer(Float[] v) {
+		// System.out.println("début épurer");
+		Float mem = (float) 0;
+		int indice = -1;
+		for (int i = 0; i < v.length; i++) {
+			if (v[i] - mem > 0.01) {
+				mem = v[i];
+				indice = i + 2;
+			} else {
+				v[i] = (float) 0;
+			}
+			// System.out.println(v[i]);
 		}
-		System.out.println("La longueur de la clé est " + v[i] + ".");
-		return v[i];
+
+		System.out.println("La longueur de la clé est " + indice + ".");
+		return indice;
 	}
 
 	private static HashMap<Integer, Float> remplirIC(Text t, int m) {
@@ -25,7 +39,8 @@ public class Kasiski {
 		return longueurs;
 	}
 
-	private static Integer[] estPic(HashMap<Integer, Float> hmIC) {
+	private static Float[] estPic(HashMap<Integer, Float> hmIC) {
+		// System.out.println("début estPic");
 		// Calcul du seuil
 		Float moy = (float) 0;
 		for (int i = 2; i < hmIC.size() + 2; i++) {
@@ -35,7 +50,7 @@ public class Kasiski {
 		float seuil = moy + moy * 10 / 100;
 
 		// Calcul des pics
-		Integer[] vprime = new Integer[hmIC.size()];
+		Float[] vprime = new Float[hmIC.size()];
 		for (int i = 2; i < hmIC.size() + 2; i++) {
 			Float pred;
 			Float succ;
@@ -51,28 +66,14 @@ public class Kasiski {
 				succ = (float) 0;
 			}
 			if (nb > seuil && nb > pred && nb > succ) {
-				vprime[i - 2] = i;
+				vprime[i - 2] = nb;
 			} else {
-				vprime[i - 2] = 0;
+				vprime[i - 2] = (float) 0;
 			}
+			// System.out.println(vprime[i - 2]);
 		}
 
 		return vprime;
-	}
-
-	private static int getMaxLongueurCle(Text t, int m,
-			HashMap<Integer, Float> hmIC) {
-		float maxIC = 0;
-		int maxlsc = 0;
-		Float k;
-		for (int i = 2; i < m + 1; i++) {
-			k = hmIC.get(i);
-			if (k > maxIC) {
-				maxIC = k;
-				maxlsc = i;
-			}
-		}
-		return maxlsc;
 	}
 
 	// 2 : Trouver une liste de clés possibles
