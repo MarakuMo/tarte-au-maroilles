762b6d34a20b56b5f7b450479d823d46a8cbe3e1
auberi.lecocq <auberi.lecocq@gmail.com>
2011-05-14 20:13:32 +0000

    Calcul de la longueur de la clé : OK.

--- projet/RO/src/Test.java
+++ projet/RO/src/Test.java
@@ -7,7 +7,7 @@ public class Test {
 				"c",
 				"/home/auberi/Documents/Recherche_Operationnelle_projet/Ref0123",
 				"/home/auberi/Documents/Recherche_Operationnelle_projet/texte_chiffre.txt",
-				"bidule" };
+				"amies" };
 		vigenere.main(arg);
 		// String[] arg2 = new String[] {
 		// "d",
@@ -18,23 +18,8 @@ public class Test {
 
 		Text t = new Text(
 				"/home/auberi/Documents/Recherche_Operationnelle_projet/texte_chiffre.txt");
-		float i = Text.indiceCoincidence(t.getContenu());
-		System.out.println("IC du texte entier = " + i + "\n");
-		float max = 0;
-		int maxlsc = 0;
-		for (int lsc = 25; lsc > 2; lsc--) {
-			String s = t.sousChaine(lsc);
-			float k = Text.indiceCoincidence(s);
-			if (k > max) {
-				max = k;
-				maxlsc = lsc;
-			}
-			System.out.println(lsc + " max = " + k);
-		}
-		System.out.println("max = " + max);
-		System.out.println("maxlsc = " + maxlsc);
 		// System.out.println(t.getContenu());
 		// vigenere v = new vigenere();
-
+		Kasiski.longueurCle(t, 15);
 	}
 }
